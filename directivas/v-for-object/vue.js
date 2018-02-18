b3cf00e2af9b81296d1220c3465a293d1de10b7f
Aldo Choque <achoque1400@gmail.com>
2018-02-18 11:27:44 -0500

    v-for con objetos

---
+++ directivas/v-for-object/vue.js
@@ -0,0 +1,13 @@
+var app=new Vue({
+  el:'#vn',
+  data:{
+    empleado:{
+      nombre:'Carlos Castro',
+      edad:'30 años',
+      nacionalidad:'Mexicano',
+      profesion:'Programador',
+      empresa:'IBM',
+      telefono:'2532905'
+    }
+  }
+})
